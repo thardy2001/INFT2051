27121cee127de28250f9f1e244d2d309242d1dcf
tHardy7 <49541450+tHardy7@users.noreply.github.com>
2020-10-10 13:03:51 +1100

    Added begining
    English text to speech
    no speech to text'
    no other languages

---
+++ XTranslator/XTranslator/XTranslator/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace XTranslator
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void speech_to_text(object sender, System.EventArgs e)
+        {
+
+        }
+        async void text_to_speech(object sender, System.EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync(txtInput.Text);
+        }
+    }
+}
