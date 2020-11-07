6bdc7b543d70bc802f191c8d7e0c5195c019b92e
tHardy7 <49541450+tHardy7@users.noreply.github.com>
2020-11-07 14:26:40 +1100

    Added links for referencing

--- XTranslator/Test/Test/Views/HomePage.xaml.cs
+++ XTranslator/Test/Test/Views/HomePage.xaml.cs
@@ -11,8 +11,6 @@ namespace Test.Views
     public partial class HomePage : ContentPage
     {
         string text = ""; // The text to be translated 
-        string LF = "en"; // Language Source 
-        string LT = "en"; // Target Language 
 
         IDictionary<string, string> languages = new Dictionary<string, string>(){
                 {"English", "en"},
@@ -24,31 +22,32 @@ namespace Test.Views
                 {"Japanese", "ja"},
                 {"Auto-Detect", "auto"}
         };
+
         void EditorTextChanged(object sender, TextChangedEventArgs e) {text = e.NewTextValue;} // Update the text whenever a change occurs 
         private void translateButton_Click(object sender, EventArgs e)
         {
-            if(LF == "auto"){
-                TranslateText(text, DetectLanguage(), LT);
+            if(languages[(string)languageFrom.SelectedItem] == "auto"){
+                TranslateText(text, DetectLanguage(), languages[(string)languageTo.SelectedItem]);
             } else
             {
-                TranslateText(text, LF, LT);
+                TranslateText(text, languages[(string)languageFrom.SelectedItem], languages[(string)languageTo.SelectedItem]);
             }
             
         }
-        public string DetectLanguage()
+        public string DetectLanguage() // https://cloud.google.com/translate/docs/basic/detecting-language
         {
             TranslationClient client = TranslationClient.Create();
             var detection = client.DetectLanguage(text: text);
             return detection.Language;
         }
-        public string TranslateText(string t, string target, string source)
+        public string TranslateText(string t, string target, string source) //https://cloud.google.com/translate/docs/basic/translating-text
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             TranslationClient client = TranslationClient.Create();
             var response = client.TranslateText(
                 text: t,
-                targetLanguage: target,  // Russian  ru
-                sourceLanguage: source);  // English en 
+                targetLanguage: target, 
+                sourceLanguage: source);  
             translated.Text = response.TranslatedText;
             return response.TranslatedText;
         }
